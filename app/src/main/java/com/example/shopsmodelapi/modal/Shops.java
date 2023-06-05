bcd46e7c6240dae969b8910349185a364bfde037
Arigela sriraghu <sriraghu.arigela@improve10x.com>
2023-06-05 19:25:02 +0530

    modify test cases

--- app/src/main/java/com/example/shopsmodelapi/modal/Shops.java
+++ app/src/main/java/com/example/shopsmodelapi/modal/Shops.java
@@ -1,9 +1,10 @@
 package com.example.shopsmodelapi.modal;
 
 import com.example.shopsmodelapi.ClientDetails;
-import com.example.shopsmodelapi.money.CurrentSubTotalPrice;
-import com.example.shopsmodelapi.money.CurrentTotalDiscountsSet;
+import com.example.shopsmodelapi.money.CurrentTotalPriceSet;
 import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
 
 public class Shops {
     @SerializedName("_id")
@@ -61,11 +62,107 @@ public class Shops {
     private String currentSubtotalPrice;
 
     @SerializedName("current_subtotal_price_set")
-    public CurrentSubTotalPrice currentSubTotalPrice;
+    public CurrentTotalPriceSet currentSubTotalPrice;
 
     @SerializedName("current_total_discounts")
     public String currentTotalDiscounts;
 
     @SerializedName("current_total_discounts_set")
-    public CurrentTotalDiscountsSet currentTotalDiscountsSet;
+    public CurrentTotalPriceSet currentTotalDiscountsSet;
+
+    @SerializedName("current_total_duties_set")
+    private String currentTotalDutiesSet;
+
+    @SerializedName("current_total_price")
+    private String currentTotalPrice;
+
+    @SerializedName("current_total_price_set")
+    public CurrentTotalPriceSet currentTotalPriceSet;
+
+    @SerializedName("current_total_tax")
+    private String currentTotalTax;
+
+    @SerializedName("current_total_tax_set")
+    public CurrentTotalPriceSet currentTotalTaxSet;
+
+    @SerializedName("customer_locale")
+    private String customerLocale;
+
+    @SerializedName("device_id")
+    private String deviceId;
+
+    @SerializedName("discount_codes")
+    private ArrayList<String> discountCodes;
+
+    @SerializedName("estimated_taxes")
+    private boolean estimatedTaxes;
+
+    @SerializedName("financial_status")
+    private String financialStatus;
+
+    @SerializedName("fulfillment_status")
+    private String fulfillmentStatus;
+
+    @SerializedName("gateway")
+    private String gateway;
+
+    @SerializedName("landing_site")
+    private String landingSite;
+
+    @SerializedName( "landing_site_ref")
+    private String landingSiteRef;
+
+    @SerializedName("location_id")
+    private String locationId;
+
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("note")
+    private String note;
+
+    @SerializedName("note_attributes")
+    private ArrayList<String> noteAttributes;
+
+    @SerializedName("number")
+    private Integer number;
+
+    @SerializedName("order_number")
+    private Integer orderNumber;
+
+    @SerializedName("order_status_url")
+    private String orderStatusUrl;
+
+    @SerializedName("original_total_duties_set")
+    private String originalTotalDutiesSet;
+
+    @SerializedName("payment_gateway_names")
+    private ArrayList<String> paymentGatewayNames;
+
+    @SerializedName("presentment_currency")
+    private String presentmentCurrency;
+
+    @SerializedName("processed_at")
+    private String processedAt;
+
+    @SerializedName("processing_method")
+    private String processingMethod;
+
+    @SerializedName("reference")
+    private String reference;
+
+    @SerializedName("referring_site")
+    private String referringSite;
+
+    @SerializedName( "source_identifier")
+    private String sourceIdentifier;
+
+    @SerializedName("source_name")
+    private String sourceName;
+
+    @SerializedName("source_url")
+    private String sourceUrl;
+
+    @SerializedName("subtotal_price")
+    private String subtotalPrice;
 }
--- app/src/main/java/com/example/shopsmodelapi/money/CurrentTotalPriceSet.java
+++ app/src/main/java/com/example/shopsmodelapi/money/CurrentTotalPriceSet.java
@@ -2,7 +2,7 @@ package com.example.shopsmodelapi.money;
 
 import com.google.gson.annotations.SerializedName;
 
-public class CurrentSubTotalPrice {
+public class CurrentTotalPriceSet {
     @SerializedName("shop_money")
     public ShopMoney shopMoney;
 
